eb3943f305cfb80c4c69c13223794d376dee5dbe
kirandr26 <49637329+kirandr26@users.noreply.github.com>
2020-08-05 14:25:15 +0530

    Update init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -7,5 +7,16 @@
 
 	// $(".section_wrap").niceScroll({cursorwidth: '10px', autohidemode: true, zindex: 999 });
 
+	$('.burgur-menu').click(function(){
 
+		$('.mob-links').toggle();
+		// $('.burgur-menu span.m-top').css('display','none');
+		// $('.burgur-menu span.middle').css({transform:'rotate(-45deg)',top:'30px',left:'-20px'});
+		// $('.burgur-menu span.bottom').css({transform:'rotate(45deg)',top:'00px',left:'10px'});
+
+	});
+	$('.mob-links a').click(function(){
+		$('.mob-links').css('display','none');
+		$('.mob-links ul').css('height','0%');
+	});
 // }
